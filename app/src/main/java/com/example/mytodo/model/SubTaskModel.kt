208ec2ad26a7235841a911f66fcbec6e0fa3dc8b
Vaishali <vaishali.c@elaunchinfotech.in>
2023-12-14 16:11:03 +0530

    Revert "Initial commit"
    This reverts commit bc7c9a56221802cec68a18085d3f097c1fff2f0f.

--- app/src/main/java/com/example/mytodo/model/SubTaskModel.kt
+++
@@ -1,5 +0,0 @@
-package com.example.mytodo.model
-
-import java.io.Serializable
-
-data class SubTaskModel(var isComplete: Boolean = false, var subTask: String = "") : Serializable
--- app/src/main/java/com/example/mytodo/utils/SubTaskClickListener.kt
+++
@@ -1,6 +0,0 @@
-package com.example.mytodo.utils
-
-interface SubTaskClickListener {
-    fun onItemClick(position: Int)
-
-}
